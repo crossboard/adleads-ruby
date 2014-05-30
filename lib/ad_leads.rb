a4824bb14d36ff1e5525114bde38d660e28a5f70
Christopher K Lee <cklee@constantcontact.com>
2014-05-30 15:50:29 -0400

    Pre-gem release clean up

--- lib/ad_leads.rb
+++ lib/ad_leads.rb
@@ -3,7 +3,6 @@ require 'ad_leads/version'
 require 'ad_leads/configuration'
 require 'ad_leads/client'
 require 'ad_leads/token'
-require 'ad_leads/campaign'
 
 module AdLeads
   extend Configuration
